a156aec358b63ea96d8f678742a60ec6ee62a7b1
Aditya Prabhu <guru.adu05@gmail.com>
2024-01-15 01:14:02 +0530

    added userinput, dotenv

--- github.py
+++ github.py
@@ -1,28 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import threading
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # enable headless mode in Selenium
 options = Options()
 options.add_argument('--headless=new')
 
-def scrape_github_readme(repository_name):
+
+def scrape_github_readme(username,repo_name):
     # Set up Chrome WebDriver (adjust the executable_path accordingly)
     driver = webdriver.Chrome(options = options)
-
     try:
-        repository_url = f'https://github.com/Terminal127/{repository_name}'
-        print(f"Navigating to the GitHub repository: {repository_url}")
+        repository_url = f'https://github.com/{username}/{repo_name}'
         driver.get(repository_url)
-        time.sleep(10)  # Adjust sleep duration if needed
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.markdown-body')))
+        #time.sleep(10)  # Adjust sleep duration if needed
 
-        # Find the README content
+        # Find the README content 
         readme_content = driver.find_element(By.CSS_SELECTOR, '.repository-content .markdown-body')
-
-        # Save the README content to the output file
+        
         with open("final_github_output.txt", "a" if open("final_github_output.txt").read() else "w") as output_file:
             # Check if file exists, use "a" for append, "w" for create
-            output_file.write(f"--- Repository: {repository_name} ---\n")
+            output_file.write(f"--- Repository: {repo_name.strip()} ---\n")
             output_file.write(readme_content.text + "\n\n")
             output_file.write("-" * 50 + "\n\n")
 
@@ -31,23 +36,33 @@ def scrape_github_readme(repository_name):
 
     finally:
         # Close the browser
-        print("Closing the browser...")
         driver.quit()
 
-def scrape_readmes_from_file(file_path):
+def scrape_readmes_from_file(file_path,username):
+    print(f"Navigating to given GitHub repository(s)...")
     try:
         with open(file_path, 'r') as file:
             # Read repository names line by line
             repository_names = file.readlines()
 
+            threads = []
             for name in repository_names:
+                t = threading.Thread(target=scrape_github_readme, args=[username,name])
+                t.start()
+                threads.append(t)
                 # Call the function to scrape README for each repository
-                scrape_github_readme(name.strip())
 
     except Exception as e:
         print(f"Error reading file: {e}")
+    for t in threads: 
+        t.join()
 
-# Example: File containing repository names
-file_path = 'github_repositories.txt'
-scrape_readmes_from_file(file_path)
+def main():
+    # Example: File containing repository names
+    file_path = 'github_repositories.txt'
+    username = os.environ.get("NAME")
+    scrape_readmes_from_file(file_path,username)
+    print("Closing the browser(s)...")
 
+if __name__ == '__main__':
+    main()
